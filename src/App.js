92e91bcc3568090fffb2d323e7ffe8d2250ba8e2
Zachary Deacon <zackdeacon347@gmail.com>
2020-10-06 20:46:17 -0700

    added react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Rolldice from './components/Roll Dice component/rolldice'
 import Coinflip from './components/Coin Flip Component/coinflip'
 import './App.css';
 
 function App() {
   return (
+    <>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Rolldice />
+        </Route>
+        <Route exact path="/coin">
     <Coinflip />
+    </Route>
+    </Switch>
+    </Router>
+    </>
   );
 }
 
