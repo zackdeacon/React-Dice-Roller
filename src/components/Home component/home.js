62dfdc5085d53e4c0d5cba74b178bdc43598d890
Zachary Deacon <zackdeacon347@gmail.com>
2020-10-09 18:21:36 -0700

    react router functional

--- src/components/Home component/home.js
+++ src/components/Home component/home.js
@@ -1,15 +1,26 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import "./home.css"
 
 export default function Home() {
+    const history = useHistory();
+
+    const navigateCoin = () => {
+        history.push("/coin")
+    }
+
+    const navigateRolldice = () => {
+        history.push("/Rolldice")
+    }
+
 
     return (
         <>
-        <div>
-            <button href="/Rolldice">Roll Some Dice!</button>
+        <div className="RollDiceBtnDiv">
+            <button className="btn-light btnClasses" onClick={navigateRolldice}>Roll Some Dice!</button>
         </div>
-        <div>
-            <button href="/coin">Flip a Coin!</button>
+        <div className="coinBtnDiv">
+            <button className="btn-light btnClasses" onClick={navigateCoin}>Flip a Coin!</button>
         </div>
         </>
     )
