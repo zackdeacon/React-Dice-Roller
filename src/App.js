c5a6c8e4a5afbed8cdaed6fb96c516a697a4aa75
Zachary Deacon <zackdeacon347@gmail.com>
2020-10-09 17:49:05 -0700

    added home component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Rolldice from './components/Roll Dice component/rolldice'
 import Coinflip from './components/Coin Flip Component/coinflip'
+import Home from './components/Home component/home'
 import './App.css';
 
 function App() {
@@ -10,11 +11,14 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/">
-          <Rolldice />
+          <Home />
         </Route>
         <Route exact path="/coin">
-    <Coinflip />
-    </Route>
+          <Coinflip />
+        </Route>
+        <Route exact path="/Rolldice">
+          <Rolldice />
+        </Route>
     </Switch>
     </Router>
     </>
---
+++ src/components/Home component/home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./home.css"
+
+export default function Home() {
+
+    return (
+        <>
+        <div>
+            <button href="/Rolldice">Roll Some Dice!</button>
+        </div>
+        <div>
+            <button href="/coin">Flip a Coin!</button>
+        </div>
+        </>
+    )
+}
